e50bcf9414ed6de64f4fd91aa86507f830f80876
eedu7 <mueedahmad067@gmail.com>
2026-05-09 21:50:45 +0500

    feat(core): Add CRUD methods to BaseController
    Implements core Create, Read (get_by_uid, get_all), Update, and Delete (CRUD) functionality within `BaseController`.
    
    This significantly improves the base controller class by providing standardized asynchronous methods for interacting with the database via associated repositories.
    
    The methods utilize:
    - `get_by_uid`: Retrieves an object by its unique identifier (UUID).
    - `get_all`: Fetches a paginated sequence of all objects.
    - `create`: Creates a new object and commits the transaction.
    - `update`: Updates an existing object identified by UUID, and commits the transaction.
    - `delete`: Deletes an object identified by UUID, and commits the transaction.
    - `commit`: Explicitly commits the repository session.
    
    This encapsulates common persistence logic, making controller implementations cleaner and promoting consistency across the application.

--- backend/core/controller/base.py
+++ backend/core/controller/base.py
@@ -1,4 +1,5 @@
-from typing import Generic, Type, TypeVar
+from typing import Any, Dict, Generic, Sequence, Type, TypeVar
+from uuid import UUID
 
 from app.models import DBBase
 from core.repository import BaseRepository
@@ -10,3 +11,38 @@ class BaseController(Generic[ModelType]):
     def __init__(self, model: Type[ModelType], repository: BaseRepository) -> None:
         self.model = model
         self.repository = repository
+
+    async def get_by_uid(self, uid: UUID) -> ModelType:
+        obj = await self.repository.get_by_uid(uid)
+
+        if obj is None:
+            # TODO: raise not found error
+            raise
+        return obj
+
+    async def get_all(self, offset: int = 0, limit: int = 100) -> Sequence[ModelType]:
+        response = await self.repository.get_all(offset=offset, limit=limit)
+        return response
+
+    async def create(self, attributes: Dict[str, Any] = {}) -> ModelType:
+        db_obj = await self.repository.create(attributes=attributes)
+        await self.commit()
+        return db_obj
+
+    async def delete(self, uid: UUID) -> bool:
+        db_obj = await self.get_by_uid(uid)
+
+        await self.repository.delete(db_obj)
+        await self.commit()
+        return True
+
+    async def update(self, uid: UUID, attributes: Dict[str, Any] = {}) -> ModelType:
+        db_obj = await self.get_by_uid(uid)
+
+        for key, value in attributes.items():
+            setattr(db_obj, key, value)
+        await self.commit()
+        return db_obj
+
+    async def commit(self) -> None:
+        await self.repository.session.commit()
